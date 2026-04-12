aaea93093448f93744a92be7eafaffe0d9242b92
styu12 <styu12@naver.com>
2026-04-11 22:50:33 -0700

    feat: add SessionError and StreamError types

---
+++ Sources/SyncField/Errors.swift
@@ -0,0 +1,39 @@
+// Sources/SyncField/Errors.swift
+import Foundation
+
+public enum SessionError: Error, CustomStringConvertible {
+    case invalidTransition(from: SessionState, to: SessionState)
+    case duplicateStreamId(String)
+    case noStreamsRegistered
+    case startFailed(cause: Error, rolledBack: [String])
+    case notRunning
+
+    public var description: String {
+        switch self {
+        case .invalidTransition(let from, let to):
+            return "SessionError: cannot transition from \(from) to \(to)"
+        case .duplicateStreamId(let id):
+            return "SessionError: duplicate streamId '\(id)'"
+        case .noStreamsRegistered:
+            return "SessionError: no streams registered"
+        case .startFailed(let cause, let rolledBack):
+            return "SessionError: startRecording failed (\(cause)); rolled back \(rolledBack)"
+        case .notRunning:
+            return "SessionError: operation requires the session to be running"
+        }
+    }
+}
+
+public struct StreamError: Error, CustomStringConvertible {
+    public let streamId: String
+    public let underlying: Error
+
+    public init(streamId: String, underlying: Error) {
+        self.streamId = streamId
+        self.underlying = underlying
+    }
+
+    public var description: String {
+        "StreamError[\(streamId)]: \(underlying)"
+    }
+}
